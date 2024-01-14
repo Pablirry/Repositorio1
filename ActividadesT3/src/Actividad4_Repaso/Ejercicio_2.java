ff9a7a452758e3d69c07319166dbe3acb1c4e4dc
Pablirry <pablo.lopez02@csioviedo.com>
2024-01-14 14:52:11 +0100

    .

--- ActividadesT3/src/Actividad4_Repaso/Ejercicio_2.java
+++ ActividadesT3/src/Actividad4_Repaso/Ejercicio_2.java
@@ -3,13 +3,18 @@ package Actividad4_Repaso;
 public class Ejercicio_2 {
 
 	public static void main(String[] args) {
-		
+
 		/*
 		 * Implemente un método llamado secuencia que realice la búsqueda de la
 		 * secuencia en orden creciente más larga dentro de un vector de enteros. El
 		 * método ha de mostrar tanto la posición de la primera componente de la
 		 * secuencia como el tamaño de la misma.
 		 */
+
+		int[] vector = { 4, 8, 4, 2, 3, 5, 7, 1, 6 };
+
+		Libreria.mostrarVector(vector);
+		Libreria.secuencia(vector);
 
 	}
 
--- ActividadesT3/src/Actividad4_Repaso/Libreria.java
+++ ActividadesT3/src/Actividad4_Repaso/Libreria.java
@@ -4,34 +4,63 @@ public class Libreria {
 
     /**
      * Inserta un valor en el vector
-     * @param v : entero[]
-     * @param valor : entero 
-     * @param pos : entero
+     * 
+     * @param v     : entero[]
+     * @param valor : entero
+     * @param pos   : entero
      */
 
     public static void insertaValor(int[] v, int valor, int pos) {
-        if(pos<0 || pos>=v.length){
+        if (pos < 0 || pos >= v.length) {
             System.out.println("ERROR fuera de rango");
             return;
         }
-        for(int i=v.length-2; i>=pos; i--){
-            v[i+1]=v[i];
+        for (int i = v.length - 2; i >= pos; i--) {
+            v[i + 1] = v[i];
         }
-        v[pos]=valor;
-		
-	}
+        v[pos] = valor;
+
+    }
 
     /**
      * Funcion para mostrar un vector
+     * 
      * @param v : entero[]
      */
 
-	public static void mostrarVector(int[] v) {
-        for(int i=0; i<v.length; i++){
-            System.out.print("["+i+"]:"+v[i]+" ");
+    public static void mostrarVector(int[] v) {
+        for (int i = 0; i < v.length; i++) {
+            System.out.print("[" + i + "]:" + v[i] + " ");
         }
         System.out.println();
-		
-	}
+
+    }
+
+    public static void secuencia(int[] v) {
+        int longActual = 1;
+        int posIncio = 0;
+        int maxLong = 1;
+        int maxPosIncio = 0;
+
+        for (int i = 1; i < v.length; i++) {
+            if (v[i] > v[i - 1]) {
+                longActual++;
+            } else {
+                if (longActual > maxLong) {
+                    maxLong = longActual;
+                    maxPosIncio = posIncio;
+                }
+                longActual = 1;
+                posIncio = i;
+            }
+        }
+        if (longActual > maxLong) {
+            maxLong = longActual;
+            maxPosIncio = posIncio;
+        }
+        System.out.println(
+                "La secuencia mas larga es de longitud " + maxLong + " y comienza en la posicion " + maxPosIncio);
+
+    }
 
 }
